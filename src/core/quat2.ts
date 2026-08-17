de1c8feebf90dbdfcb60da88cbc37a56d1849511
Isaac Mason <isaac@isaacmason.com>
2026-08-17 12:27:44 +1000

    refactor(quat2): return literal directly from fromRotationTranslationValues

--- src/core/quat2.ts
+++ src/core/quat2.ts
@@ -81,19 +81,19 @@ export function fromRotationTranslationValues(
     y2: number,
     z2: number,
 ): Quat2 {
-    const dq = create();
-    dq[0] = x1;
-    dq[1] = y1;
-    dq[2] = z1;
-    dq[3] = w1;
     const ax = x2 * 0.5;
     const ay = y2 * 0.5;
     const az = z2 * 0.5;
-    dq[4] = ax * w1 + ay * z1 - az * y1;
-    dq[5] = ay * w1 + az * x1 - ax * z1;
-    dq[6] = az * w1 + ax * y1 - ay * x1;
-    dq[7] = -ax * x1 - ay * y1 - az * z1;
-    return dq;
+    return [
+        x1,
+        y1,
+        z1,
+        w1,
+        ax * w1 + ay * z1 - az * y1,
+        ay * w1 + az * x1 - ax * z1,
+        az * w1 + ax * y1 - ay * x1,
+        -ax * x1 - ay * y1 - az * z1,
+    ];
 }
 
 /**
