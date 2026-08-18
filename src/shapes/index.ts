25fe280097274a9d9e865285c012c8ca3d35ec35
Isaac Mason <isaac@isaacmason.com>
2026-08-18 10:35:47 +1000

    feat(triangle2): add 2D triangle area, centroid, bounds, and containsPoint

--- src/shapes/index.ts
+++ src/shapes/index.ts
@@ -24,6 +24,8 @@ export * as segment2 from './segment2';
 
 export * as polygon2 from './polygon2';
 
+export * as triangle2 from './triangle2';
+
 export * as triangle3 from './triangle3';
 
 export * as raycast3 from './raycast3';
